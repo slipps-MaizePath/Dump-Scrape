463d8907e075584f2d3a47b3036b9155eb9447bc
slin63 <slin63@illinois.edu>
2016-05-24 10:09:04 -0500

    Field map generator now dynamically reads and generates maps without needing to be passed field_name arguments

--- rowswap/fieldmaps.py
+++ rowswap/fieldmaps.py
@@ -1,22 +1,25 @@
+# http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.html
+# slin63@illinois.edu
 import pandas
 
-#import data
-dborow = pandas.read_csv('dbo_row_sl_revision5-23-2016.csv', delimiter=',')
-print(dborow.columns)
-passes=pandas.read_csv('pass.csv', delimiter=',')
 
-#subset data by field
-sf541=dborow.query('Field_Name == "541-49"')
-cfar600=dborow.query('Field_Name == "CFAR600"')
+def generate_map(key_csv, pass_csv):
+    passes = pandas.read_csv(pass_csv, delimiter=',')
 
-#make map
-fieldmapsf541=sf541.pivot(index='Row', columns='Range', values='Row_ID')
-fieldmapcfar600=cfar600.pivot(index='Row', columns='Range', values='Row_ID')
+    dborow = pandas.read_csv(key_csv, delimiter=',')
+    field_set = set(dborow.get(key='Field_Name'))  # field_set contains all Field_Names listed in the passed CSV
 
-#to count number of ranges: sf541numranges=sf541['Range'].value_counts()
+    for field in field_set:
+        query_string = 'Field_Name == "{0}"'.format(field)
+        file_string = 'fieldmap[{0}].csv'.format(field)
 
-fieldmapsf541pass=pandas.concat([fieldmapsf541, passes], axis=1)
-fieldmapcfar600pass=pandas.concat([fieldmapcfar600, passes], axis=1)
+        field_df = dborow.query(query_string)  # Subsetting data
+        field_df = field_df.pivot(index='Row', columns='Range', values='Row_ID')  # Making the map
+        field_df_passes = pandas.concat([field_df, passes], axis=1)  # Appending pass axes
+        field_df_passes.to_csv(file_string, sep=',')  # Outputting data
 
-fieldmapsf541pass.to_csv('fieldmapsf541.csv', sep=',')
-fieldmapcfar600pass.to_csv('fieldmapcfar600.csv', sep=',')
+        print('Output as: {}'.format(file_string))
+
+
+if __name__ == "__main__":
+    generate_map(key_csv='dbo_row_sl_revision5-23-2016.csv', pass_csv='pass.csv')
