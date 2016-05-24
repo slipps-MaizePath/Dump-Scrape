c27bd51e65c81e02056a3fbd979788cfc594914b
slin63 <slin63@illinois.edu>
2016-05-24 15:09:39 -0500

    Ironed out bugs

--- rowrangepopulator/rowrangepopulator.py
+++ rowrangepopulator/rowrangepopulator.py
@@ -24,25 +24,27 @@ def populate(csvfile, max_row=10, max_range=5, start_row=5, start_range=2):
             coords = generate_coords(  # Ordered (range, row)
                 max_row, max_range, start_range=start_range, start_row=start_row
             )
-
-            for row in reader:
-                writer.writerow({
-                    'Row_ID': row['Row_ID'],
-                    'Experiment_name': row['Experiment_name'],
-                    'Source_Seed': row['Source_Seed'],
-                    'Pedigree': row['Pedigree'],
-                    'Field_Name': row['Field_Name'],
-                    'Row_name': row['Row_name'],
-                    'Range': coords[index][0],
-                    'Row': coords[index][1],
-                    'Block': row['Block'],
-                    'Rep': row['Rep'],
-                    'Kernel_Num': row['Kernel_Num'],
-                    'Planting_Date': row['Planting_Date'],
-                    'Harvest_Date': row['Harvest_Date'],
-                    'Row_Comments': row['Row_Comments']
-                })
-                index += 1
+            try:
+                for row in reader:
+                    writer.writerow({
+                        'Row_ID': row['Row_ID'],
+                        'Experiment_name': row['Experiment_name'],
+                        'Source_Seed': row['Source_Seed'],
+                        'Pedigree': row['Pedigree'],
+                        'Field_Name': row['Field_Name'],
+                        'Row_name': row['Row_name'],
+                        'Range': coords[index][0],
+                        'Row': coords[index][1],
+                        'Block': row['Block'],
+                        'Rep': row['Rep'],
+                        'Kernel_Num': row['Kernel_Num'],
+                        'Planting_Date': row['Planting_Date'],
+                        'Harvest_Date': row['Harvest_Date'],
+                        'Row_Comments': row['Row_Comments']
+                    })
+                    index += 1
+            except IndexError:
+                print '\tAllotted space too small for complete planting.\n\tRows and ranges filled where possible.'
 
             print 'Output at: {1}/{0}'.format('OUT-' + csvfile, getcwd())
 
@@ -61,8 +63,6 @@ def generate_coords(max_row, max_range, start_range, start_row):
     while current_range <= max_range:
         coordinate_tuple += ((current_range, current_row),)
 
-        # print coordinate_tuple
-
         if increment:
             current_row += 1
         else:
@@ -72,14 +72,15 @@ def generate_coords(max_row, max_range, start_range, start_row):
             increment = False
             current_row -= 1
             current_range += 1
-        if current_row < start_row:
+        if current_row < MINIMUM_ROW:
             increment = True
             current_row += 1
             current_range += 1
-
+    print coordinate_tuple
     return coordinate_tuple
 
 
 if __name__ == '__main__':
-    file_name, csvfile, max_row, max_range = argv
-    populate(csvfile, int(max_row), int(max_range))
+    MINIMUM_ROW = 5
+    file_name, csvfile, max_row, max_range, start_row, start_range = argv
+    populate(csvfile, int(max_row), int(max_range), start_row=int(start_row), start_range=int(start_range))
