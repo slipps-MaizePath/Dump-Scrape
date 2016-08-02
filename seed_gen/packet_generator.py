95e83d270063e95c510ef0296f271055b3b77da7
Shean Lin <slin63@illinois.edu>
2016-08-02 16:32:29 -0500

    Revised for new data collection standard

--- seed_gen/packet_generator.py
+++ seed_gen/packet_generator.py
@@ -3,37 +3,38 @@
 # slin63@illinois.edu
 
 from django.http import HttpResponse
-from lab.models import ObsPlot, Experiment
+from lab.models import ObsPlot, Experiment, ObsTracker
 import csv
 
-HEADER = ['Seed ID', 'Pedigree', 'Plot ID', 'Plot Name', 'Plot', 'Block', 'Rep', 'Kernel Num', 'Planting Date', 'Harvest Date']
+HEADER = ['Seed ID', 'Pedigree', 'Researcher', 'Plot ID', 'Plot Name', 'Plot', 'Block', 'Rep', 'Kernel Num', 'Planting Date', 'Harvest Date']
 
 
 class SeedPacket(object):
-    def __init__(self, seed_id, pedigree, plot_info):
+    def __init__(self, seed_id, pedigree, trailing_info):
         self.seed_id = seed_id
         self.pedigree = pedigree
-        self.plot_info = plot_info
+        self.trailing_info = trailing_info
 
     def get_values(self):
-        return (self.seed_id, self.pedigree) + self.plot_info
+        return (self.seed_id, self.pedigree) + self.trailing_info
 
     def __repr__(self):
-        return self.seed_id + ' ' + self.plot_info
+        return self.seed_id + ' ' + self.trailing_info
 
 
-def seed_list_make(poll_dict):
+def seed_list_make(polli_objs):
     seed_list = []
-    for e in poll_dict:
-        split_val = poll_dict[e].split(':')
-        poll_type = split_val[0]
-        poll_range = split_val[1]
+    for e in polli_objs:
+        polli_type = e.polli_type
+        polli_range = e.polli_count
         seed_pedigree = get_pedigree('#TODO')
-        plot_info = ObsPlot.objects.values_list('plot_id', 'plot_name', 'plot', 'block', 'rep', 'kernel_num', 'planting_date', 'harvest_date').get(id=e.id)
+        researcher = ObsTracker.objects.values_list('user__first_name', 'user__last_name').get(id=e.obs.id)
+        trailing_info = (' '.join(researcher),)
+        trailing_info += ObsPlot.objects.values_list('plot_id', 'plot_name', 'plot', 'block', 'rep', 'kernel_num', 'planting_date', 'harvest_date').get(id=e.obs.obs_plot.id)
 
-        for num in xrange(1, int(poll_range) + 1):
-            seed_id = seed_name_make(plot_id=e.plot_id, num=num, poll_type=poll_type)
-            seed_list.append(SeedPacket(seed_id=seed_id, pedigree=seed_pedigree, plot_info=plot_info))
+        for num in xrange(1, int(polli_range) + 1):
+            seed_id = seed_name_make(plot_id=e.obs.obs_plot.plot_id, num=num, polli_type=polli_type)
+            seed_list.append(SeedPacket(seed_id=seed_id, pedigree=seed_pedigree, trailing_info=trailing_info))
 
     return seed_list
 
@@ -42,11 +43,17 @@ def get_pedigree(seed_id):
     return '##TODO'
 
 
-def seed_name_make(plot_id, num, poll_type):
-    if num < 10:
-        num_str = "{}_00{}{}".format(plot_id, num, poll_type[0])
+def seed_name_make(plot_id, num, polli_type):
+    if polli_type.lower() == 'cross':
+        polli_type = 'b'
     else:
-        num_str = "{}_0{}{}".format(plot_id, num, poll_type[0])
+        polli_type = 's'
+
+    if num < 10:
+        num_str = "{}_00{}{}".format(plot_id, num, polli_type)
+    else:
+        num_str = "{}_0{}{}".format(plot_id, num, polli_type)
+
     return num_str
 
 
@@ -60,6 +67,4 @@ def seed_list_to_csv(exp_id, seed_list):
         writer.writerow(row.get_values())
 
     return response
-
-
 
--- seed_gen/views_packet.py
+++ seed_gen/views_packet.py
@@ -2,21 +2,35 @@ from collections import OrderedDict
 from lab.models import ObsTracker, Measurement
 from applets import packet_generator
 
-def generate_packets(request, experiment_id):
-    measurements = Measurement.objects.filter(
-       measurement_parameter__parameter='Pollinations', obs_tracker__experiment_id=experiment_id
-    )
-    poll_dict = OrderedDict({})
-    # Get plots and their pollination measurements
-    for meas in measurements:
-        plot = meas.obs_tracker.obs_plot
-        polli = meas.value
-        poll_dict[plot] = polli
 
-    seed_list = packet_generator.seed_list_make(poll_dict)
+class MeasStruct(object):
+    def __init__(self, polli_count, polli_type, obs):
+        self.polli_count = polli_count
+        self.polli_type = polli_type
+        self.obs = obs
+
+
+def generate_packets(request, experiment_id):
+    self_polli = Measurement.objects.filter(
+       measurement_parameter__parameter='Self Pollination', obs_tracker__experiment_id=experiment_id
+    )
+    cross_polli = Measurement.objects.filter(
+       measurement_parameter__parameter='Cross Pollination', obs_tracker__experiment_id=experiment_id
+    )
+    polli_objs = []
+    # Get plots and their pollination measurements
+    for meas in list(self_polli) + list(cross_polli):
+        obs = meas.obs_tracker
+        polli_type = meas.measurement_parameter.parameter_type
+        polli_count = meas.value
+        if meas.value != 0:
+            polli_objs.append(MeasStruct(polli_count, polli_type, obs))
+        else:
+            pass
+
+    seed_list = packet_generator.seed_list_make(polli_objs)
     csv_response = packet_generator.seed_list_to_csv(exp_id=experiment_id, seed_list=seed_list)
 
     return csv_response
-
 
 
